d27d9a7b54a05872b1422303602cb50d3e650166
Krekoten Marjan <krekoten@intensol.com>
2010-04-20 15:30:42 +0300

    Applied patch from JamesHarrison (http://gist.github.com/72487 line 16)

--- lib/cash/query/abstract.rb
+++ lib/cash/query/abstract.rb
@@ -12,7 +12,7 @@ module Cash
       end
 
       def perform(find_options = {}, get_options = {})
-        # Applied path from JamesHarrison (http://gist.github.com/72487 line 16)
+        # Applied patch from JamesHarrison (http://gist.github.com/72487 line 16)
         if @active_record != Delayed::Job and cache_config = cacheable?(@options1, @options2, find_options)
           cache_keys, index = cache_keys(cache_config[0]), cache_config[1]
 
